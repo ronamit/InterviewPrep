ec076caf933a2a3a3ae9f6393533adbfc37d9c76
ronamit <amitron5@gmail.cpm>
2020-10-22 17:58:42 +0300

    r

---
+++ LeetCode/Solutions_2020/1600. Throne Inheritance.py
@@ -0,0 +1,68 @@
+from typing import List, Dict, Tuple, Sequence
+import itertools, collections
+
+class Person:
+    def __init__(self, Name: str, parentName=''):
+        self.name = Name
+        self.isAlive = True
+        self.parentName = parentName
+        self.childsNames = []
+
+    def create_newborn(self, childName: str, parentName: str):
+        child = Person(childName, parentName)
+        self.childsNames = [childName] + self.childsNames # increaseing age order
+        return child
+
+
+class ThroneInheritance:
+
+    def __init__(self, kingName: str):
+        self.kingName = kingName
+        self.king = Person(kingName, '')
+        self.persons = {kingName: self.king}
+
+    def birth(self, parentName: str, childName: str) -> None:
+        parent = self.persons[parentName]
+        child = parent.create_newborn(childName, parentName)
+        self.persons[childName] = child
+
+
+    def death(self, name: str) -> None:
+        assert name in self.persons
+        person = self.persons[name]
+        person.isAlive = False
+    # end def
+
+    def getInheritanceOrder(self) -> List[str]:
+        inInhertedSet = set()
+        personName = self.kingName
+        inhert_order_living = []
+        # Run DFS
+        stk = [personName]
+        while stk:
+            personName = stk.pop()
+            person = self.persons[personName]
+            if person.isAlive:
+                inhert_order_living.append(person.name)
+            # end if
+            for childName in person.childsNames:
+                stk.append(childName)
+            # end for
+        # end while
+        return inhert_order_living
+    # end def
+
+# Your ThroneInheritance object will be instantiated and called as such:
+
+t = ThroneInheritance("king")# order: king
+t.birth("king", "andy")# order: king > andy
+t.birth("king", "bob")# order: king > andy > bob
+t.birth("king", "catherine")# order: king > andy > bob > catherine
+t.birth("andy", "matthew")# order: king > andy > matthew > bob > catherine
+t.birth("bob", "alex")# order: king > andy > matthew > bob > alex > catherine
+t.birth("bob", "asha")# order: king > andy > matthew > bob > alex > asha > catherine
+inhert_order = t.getInheritanceOrder()# return ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"]
+print(inhert_order)
+t.death("bob")# order: king > andy > matthew > bob > alex > asha > catherine
+inhert_order = t.getInheritanceOrder()# return ["king", "andy", "matthew", "alex", "asha", "catherine"]
+print(inhert_order)
