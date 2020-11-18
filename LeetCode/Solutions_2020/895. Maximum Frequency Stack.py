729acfaf9401d8678f33e3bc90966b2286b297bc
ronamit <amitron5@gmail.cpm>
2020-11-18 22:32:41 +0200

    r

--- LeetCode/Solutions_2020/895. Maximum Frequency Stack.py
+++ LeetCode/Solutions_2020/895. Maximum Frequency Stack.py
@@ -9,13 +9,13 @@ from collections import OrderedDict
 class FreqStack:
 
     def __init__(self):
-        self.stk = []
         self.num2count = dict() # key: item, val: count
         self.count2nums = dict() # key: count, val: set of items with that count in the stack
         self.most_freq_num = None
         self.most_freq_count = 0
         self.count2smaller_cnt = dict()  # key: count of some x in stack, val: the highest count of some x in stack that is smaller than key
     # end def
+
 
     def update_state(self, x, delta):
         '''
@@ -34,38 +34,47 @@ class FreqStack:
         # clean-up memory:
         if new_cnt == 0: # x is no longer in the stack now
             del self.num2count[x]  # clean-up memory
-
+        # end if
         # update count2nums:
         if new_cnt not in self.count2nums:
             self.count2nums[new_cnt] = set()
-        self.count2nums[new_cnt].add(x)
+        # end if
+        if new_cnt > 0:
+            self.count2nums[new_cnt].add(x)
+        # end if
         # clean-up memory:
         if old_cnt in self.count2nums and len(self.count2nums[old_cnt]) == 0:
             del self.count2nums[old_cnt]
 
-        # update most freq:
-        self.count2nums[new_cnt].add(x)
-        if new_cnt > self.most_freq_count:
-            self.count2smaller_cnt[new_cnt] = self.most_freq_count
-            self.most_freq_count = new_cnt
-        # end if
-        if new_cnt == self.most_freq_count:
-            self.most_freq_num = x  # if equal we want the more recent element
+        # update count2smaller_cnt:
+        if old_cnt in self.count2smaller_cnt:
+            self.count2smaller_cnt[new_cnt] = self.count2smaller_cnt[old_cnt]
+        else:
+            self.count2smaller_cnt[new_cnt] = 0
         # end if
 
+
+        # update most freq:
+        if new_cnt >= self.most_freq_count:  # includes the equal case since we want the more recent element
+            self.count2smaller_cnt[new_cnt] = self.most_freq_count
+            self.most_freq_count = new_cnt
+            self.most_freq_num = x
+        # TODO: fix this:
+        elif delta == -1 and (self.most_freq_count in self.count2nums) and (len(self.count2nums[self.most_freq_count]) == 0):
+                self.most_freq_count = self.count2smaller_cnt[self.most_freq_count]
+                self.most_freq_num = self.count2nums[self.most_freq_count][-1]  # take the last added with this count
+            # end if
+        # end if
+        return x
     # end def
 
     def push(self, x: int) -> None:
-        # add to stack
-        self.stk.append(x)
-        # updates:
         self.update_state(x, delta=+1)
     # end def
 
     def pop(self) -> int:
         # remove from stack:
-        x = self.stk.pop()
-        # updates:
+        x = self.most_freq_num
         self.update_state(x, delta=-1)
         return x
     # end def
@@ -93,7 +102,7 @@ for i in range(len(ops_list)):
     # end if
 # end for
 # ---------------------------------------------------------------------------------------------------------------------------#
-print(out_list)
+print('Output: ', out_list)
 print('count2nums: ', stk.count2nums)
 print('most_freq_num: ', stk.most_freq_num)
 print('most_freq_count: ', stk.most_freq_count)
