6d81be8c6ff0f9ef6c604cdb4b43382772284b08
ronamit <ronamit@campus.technion.ac.il>
2022-06-27 00:11:55 +0300

    up

--- newton_lagrange.py
+++ newton_lagrange.py
@@ -26,18 +26,19 @@ def run_newton_method(n_dim, func, grad, hess, start=None, step_size=0.1, max_it
 #########################################################################################################
 
 
-def run_augmented_lagrangian_method(n_dim, m_constraints, func, f_grad, f_hess, constraints_vec, constraints_jacobian, inner_solver, n_iter=100,
+def run_augmented_lagrangian_method(n_dim, m_constraints, func, f_grad, f_hess, constraints_vec, constraints_jacobian,
+                                    x_feasiable, inner_solver, n_iter=100,
                                     eta_start=1e-3, eta_mult=1.5, lamb_step_size=0.1, verbose=1):
     # https://en.wikipedia.org/wiki/Augmented_Lagrangian_method#General_method
 
 
     lamb = np.zeros(m_constraints)
-    x = np.zeros(n_dim)
+    x = x_feasiable
     eta = eta_start
     for i in range(n_iter):
 
         if verbose:
-            print('-'* 20 + f'iteration {i}')
+            print('-' * 20 + f'iteration {i}')
             f_val = func(x)
             grad_val = f_grad(x)
             hess_val = f_hess(x)
@@ -48,14 +49,16 @@ def run_augmented_lagrangian_method(n_dim, m_constraints, func, f_grad, f_hess, 
             print(f'eta={eta}')
 
         # The augmented lagrangian objective function:
-        phi = lambda _x: func(_x) + (eta / 2) * np.sum(constraints_vec(_x) ** 2) + np.sum(lamb * constraints_vec(_x))
+        phi = lambda _x: func(_x) \
+                         + eta * np.sum(-np.log(constraints_vec(_x))) \
+                         + np.sum(lamb * constraints_vec(_x))
 
         phi_val = phi(x)
         print(f'phi_val={phi_val}')
 
         # gradient w.r.t. x is the gradient of the augmented lagrangian
         phi_grad = lambda _x: f_grad(_x) \
-                             + eta * constraints_vec(_x) @ constraints_jacobian(_x) \
+                             - eta * (1 / constraints_vec(_x)) @ constraints_jacobian(_x) \
                              + lamb @ constraints_jacobian(_x)
 
         phi_hess = lambda _x: f_hess(_x)
@@ -83,6 +86,7 @@ quadratic_func_constr_jacobian = lambda x: np.array([[-0.5, -1],
                                                     [-1, 1],
                                                     [1, 1]])
 m_constraints = 3
+x_feasiable = np.array([0, 0])
 #
 # quadratic_func_constr = lambda x: np.array([x[1] - 1 + 0.5 * x[0],
 #                                             x[1] - 1 + 0.5 * x[0]
@@ -93,7 +97,8 @@ m_constraints = 3
 # m_constraints = 2
 
 x = run_augmented_lagrangian_method(n_dim, m_constraints, quadratic_func, quadratic_func_grad, quadratic_func_hess,
-                                    quadratic_func_constr, quadratic_func_constr_jacobian, run_newton_method, n_iter=20)
+                                    quadratic_func_constr, quadratic_func_constr_jacobian, x_feasiable,
+                                    run_newton_method, n_iter=10)
 
 # x = run_newton_method(n_dim, quadratic_func, quadratic_func_grad, quadratic_func_hess)
 # print('x = ', x)
