482dd8ef674f6935cdf7398e9039fd83f730c013
ronamit <ronamit@campus.technion.ac.il>
2024-07-12 12:20:33 +0300

    up

--- LeetCode/Solutions_2024/2101. Detonate the Maximum Bombs.py
+++ LeetCode/Solutions_2024/2101. Detonate the Maximum Bombs.py
@@ -7,18 +7,18 @@ class Solution:
         n = len(bombs)
         adj_list = [[] for _ in range(n)]
         for i1 in range(n):
-            for i2 in range(i1+1, n):
+            for i2 in range(i1 + 1, n):
                 bomb1 = bombs[i1]
                 bomb2 = bombs[i2]
-                dist_sqr = (bomb1[0] - bomb2[0])**2 + (bomb1[1] - bomb2[1])**2
+                dist_sqr = (bomb1[0] - bomb2[0]) ** 2 + (bomb1[1] - bomb2[1]) ** 2
                 r1 = bomb1[2]
                 r2 = bomb2[2]
-                if r1 ** 2 >= dist_sqr:
+                if r1**2 >= dist_sqr:
                     adj_list[i1].append(i2)
-                if r2 ** 2 >= dist_sqr:
+                if r2**2 >= dist_sqr:
                     adj_list[i2].append(i1)
-        
-        # loop over the nodes and run DFS from each 
+
+        # loop over the nodes and run DFS from each
         print(adj_list)
 
         def count_explodes(i: int, exploded_already: set) -> int:
@@ -29,7 +29,7 @@ class Solution:
             for c in adj_list[i]:
                 n_explodes += count_explodes(c, exploded_already)
             return n_explodes
-                    
+
         max_explodes = 0
         for i in range(n):
             exploded_already = set()
@@ -37,7 +37,7 @@ class Solution:
         return max_explodes
 
 
-if __name__ == '__main__':
-    bombs =[[2,1,3],[6,1,4]]
+if __name__ == "__main__":
+    bombs = [[2, 1, 3], [6, 1, 4]]
     sol = Solution()
     print(sol.maximumDetonation(bombs))
