9d94fbabf0869231af32dd05bb060dbdfc48f2ec
ronamit <ronamit@campus.technion.ac.il>
2024-07-13 22:13:43 +0300

    up

--- LeetCode/Solutions_2024/1820. Maximum Number of Accepted Invitations.py
+++ LeetCode/Solutions_2024/1820. Maximum Number of Accepted Invitations.py
@@ -1,27 +1,10 @@
-# class Solution:
-#     def maximumInvitations(self, grid: List[List[int]]) -> int:
-#         m = len(grid)
-#         n = len(grid[0])
-#         boys = list(range(m))
-#         girls = list(range(n))
 
-#         def get_max(b, g):
-#             if len(b) == 0 or len(g) == 0:
-#                 return 0
-#             i = b[0]
-#             m = 0  # max matches
-#             for j_ind, j in enumerate(g):
-#                 # check if taking the pair (i,j) improves max
-#                 g_without_j = g[:j_ind] + g[(j_ind + 1) :]
-#                 max_if_match = grid[i][j] + get_max(b[1:], g_without_j)
-#                 m = max(m, max_if_match)
-#             return m
-
-#         return get_max(boys, girls)
-
+""" 
+Solution is form:
 # https://www.geeksforgeeks.org/maximum-bipartite-matching/
 # https://leetcode.com/problems/maximum-number-of-accepted-invitations/solutions/1978859/python-hungarian-algorithm-easy-to-understand/
-
+"""
+  
 from typing import List
 
 class Solution:
@@ -55,3 +38,23 @@ class Solution:
             dfs(boy, set())
 
         return len(matches)
+# class Solution:
+#     def maximumInvitations(self, grid: List[List[int]]) -> int:
+#         m = len(grid)
+#         n = len(grid[0])
+#         boys = list(range(m))
+#         girls = list(range(n))
+
+#         def get_max(b, g):
+#             if len(b) == 0 or len(g) == 0:
+#                 return 0
+#             i = b[0]
+#             m = 0  # max matches
+#             for j_ind, j in enumerate(g):
+#                 # check if taking the pair (i,j) improves max
+#                 g_without_j = g[:j_ind] + g[(j_ind + 1) :]
+#                 max_if_match = grid[i][j] + get_max(b[1:], g_without_j)
+#                 m = max(m, max_if_match)
+#             return m
+
+#         return get_max(boys, girls)
--- LeetCode/Solutions_2024/939. Minimum Area Rectangle.py
+++ LeetCode/Solutions_2024/939. Minimum Area Rectangle.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def minAreaRect(self, points: List[List[int]]) -> int:
         points = [tuple(p) for p in points]
