f151c5f56a1e3e022e47047310e9525b64188252
ronamit <ronamit@campus.technion.ac.il>
2024-07-19 10:15:49 +0300

    up

--- LeetCode/Solutions_2024/_INCOMPLETE_2025. Maximum Number of Ways to Partition an Array.py
+++ LeetCode/Solutions_2024/_INCOMPLETE_2025. Maximum Number of Ways to Partition an Array.py
@@ -1,3 +1,8 @@
+"""
+    See a good soluition here: https://leetcode.com/problems/maximum-number-of-ways-to-partition-an-array/solutions/1507271/python-cpp-explanation-with-pictures-o-n/?envType=company&envId=google&favoriteSlug=google-thirty-days
+"""
+
+
 def add_to_seen(d, x, i):
     if x not in d:
         d[x] = [i]
@@ -9,7 +14,8 @@ class Solution:
     def waysToPartition(self, nums: list[int], k: int) -> int:
 
         n = len(nums)
-        optimal_sols = set() # set of pivot: replaced index (used to avoid duplicates)
+        unchanged_sols = 0
+        change_sols = [0 for _ in range(n)]  # for each index, how many solutions are optimal if we change it
 
         # cummaltive sum
         up_sum = [None for _ in range(n)]
@@ -29,30 +35,30 @@ class Solution:
         # or if we can fix the diff with a k replacement from 0..i
         seen_nums = {nums[0]: [0]}  #  x : seem indexes
         for pivot in range(1, n):
-            diff =  down_sum[pivot]-  up_sum[pivot - 1]
+            diff = down_sum[pivot] - up_sum[pivot - 1]
             if diff == 0:
-                optimal_sols.add((pivot, None))
-                
-            if diff != 0 and (k - diff) in seen_nums:
+                unchanged_sols += 1
+            if (k - diff) in seen_nums:
                 for i in seen_nums[k - diff]:
-                    optimal_sols.add((pivot, i))
+                    change_sols[i] += 1
             add_to_seen(seen_nums, nums[pivot], pivot)
 
         # go down the indexes and see if for some i
         # or if we can fix the diff with a k replacment from i,..,n-1
         seen_nums = {}  #   x : count seen
         for pivot in range(n - 1, 0, -1):  # n-1,...,1
-            diff = up_sum[pivot]  = down_sum[pivot - 1]
-            if diff != 0 and  (k - diff) in seen_nums:
+            diff = up_sum[pivot] = down_sum[pivot - 1]
+            if (k - diff) in seen_nums:
                 for i in seen_nums[k - diff]:
-                    optimal_sols.add((pivot, i))
+                    change_sols[i] += 1
             add_to_seen(seen_nums, nums[pivot], pivot)
 
-        print(optimal_sols)
-        return len(optimal_sols)
+        ans = max(unchanged_sols, *change_sols)
+        return ans
+
 
 sol = Solution()
 # nums = [22,4,-25,-20,-15,15,-16,7,19,-10,0,-13,-14]
-nums = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30827,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+nums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30827, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 k = 0
 print(sol.waysToPartition(nums, k))
