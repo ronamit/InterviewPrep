6a6e9d3ecde5ec6190d3f4a8f445707f29300ef8
RonAmitV <121930118+RonAmitV@users.noreply.github.com>
2024-08-20 16:33:28 +0300

    up

--- LeetCode/Solutions_2024/102. Binary Tree Level Order Traversal102. Binary Tree Level Order Traversal.py
+++ LeetCode/Solutions_2024/102. Binary Tree Level Order Traversal102. Binary Tree Level Order Traversal.py
@@ -1,12 +1,12 @@
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
-
-from collections import deque
 
 
 class Solution:
