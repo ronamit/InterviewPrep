1ac7f625b784d1698f694e932812dc5bbec4cc9d
RonAmitV <121930118+RonAmitV@users.noreply.github.com>
2024-08-31 14:02:35 +0300

    up

---
+++ LeetCode/Solutions_2024/2439. Minimize Maximum of Array.py
@@ -0,0 +1,13 @@
+import math
+
+
+class Solution:
+    def minimizeArrayValue(self, nums: list[int]) -> int:
+        n = len(nums)
+        cum_sum = 0
+        ans = 0
+        for i, x in enumerate(nums):
+            cum_sum += x
+            avg_val = cum_sum / (i + 1)
+            ans = max(ans, math.ceil(avg_val))
+        return ans
