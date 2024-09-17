4fc06657251abe2df9ea7a05799db12d3822ed1f
RonAmitV <121930118+RonAmitV@users.noreply.github.com>
2024-09-17 17:31:05 +0300

    uo

--- LeetCode/Solutions_2024/884. Uncommon Words from Two Sentences.py
+++ LeetCode/Solutions_2024/884. Uncommon Words from Two Sentences.py
@@ -9,11 +9,8 @@ class Solution:
         for w, cnt1 in c1.items():
             if cnt1 == 1 and w not in c2:
                 ans.append(w)
-        
+
         for w, cnt2 in c2.items():
             if cnt2 == 1 and w not in c1:
                 ans.append(w)
         return ans
-
-            
-        
