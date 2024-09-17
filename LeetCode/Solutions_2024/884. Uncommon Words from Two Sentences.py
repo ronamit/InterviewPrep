dd33bfa8eb726df24cd4444a534273d67dc8f586
RonAmitV <121930118+RonAmitV@users.noreply.github.com>
2024-09-17 17:30:49 +0300

    up

---
+++ LeetCode/Solutions_2024/884. Uncommon Words from Two Sentences.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> list[str]:
+        c1 = Counter(s1.split())
+        c2 = Counter(s2.split())
+        ans = []
+        for w, cnt1 in c1.items():
+            if cnt1 == 1 and w not in c2:
+                ans.append(w)
+        
+        for w, cnt2 in c2.items():
+            if cnt2 == 1 and w not in c1:
+                ans.append(w)
+        return ans
+
+            
+        
